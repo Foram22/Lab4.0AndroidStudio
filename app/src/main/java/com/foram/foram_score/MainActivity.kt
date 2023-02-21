bddcfc84d71a0282b9cfc690dce565e9aff5b99c
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-20 21:40:49 -0500

    changed the design of spinner view and added toast message when switch changes the scoring team.

--- app/src/main/java/com/foram/foram_score/MainActivity.kt
+++ app/src/main/java/com/foram/foram_score/MainActivity.kt
@@ -2,17 +2,18 @@ package com.foram.foram_score
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var spinnerScoreValues: Spinner
     private lateinit var tvScoreA: TextView
     private lateinit var tvScoreB: TextView
+    private lateinit var switch: SwitchCompat
 
     // Array of integers to display score values in the spinner view
     private var scoreValues = arrayOf("Select Score Value", "1", "2", "3", "4", "5", "6")
@@ -25,23 +26,18 @@ class MainActivity : AppCompatActivity() {
         initViews()
 
         // Create the adapter to show dropdown menu
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, scoreValues)
+        val adapter = ArrayAdapter(this, R.layout.score_dropdown_menu, scoreValues)
         // Setting the adapter to the instance of Spinner View
         spinnerScoreValues.adapter = adapter
 
-        spinnerScoreValues.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-
-            }
-
-        }
-
         tvScoreA.text = "0"
         tvScoreB.text = "0"
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val message: String = if (isChecked) "You have selected Team B for scoring"
+            else "You have selected Team A for scoring"
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
 
     }
 
@@ -50,5 +46,7 @@ class MainActivity : AppCompatActivity() {
 
         tvScoreA = findViewById(R.id.tv_score_a)
         tvScoreB = findViewById(R.id.tv_score_b)
+
+        switch = findViewById(R.id.switch_to_change)
     }
 }
