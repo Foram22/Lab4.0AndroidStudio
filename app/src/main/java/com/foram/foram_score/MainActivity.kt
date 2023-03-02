9c44965f2b50d5993d7cab056909d819cf8fc70d
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-03-01 20:11:48 -0500

    added spinner functionality and view binding into the project

--- app/src/main/java/com/foram/foram_score/MainActivity.kt
+++ app/src/main/java/com/foram/foram_score/MainActivity.kt
@@ -2,53 +2,69 @@ package com.foram.foram_score
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
+import com.foram.foram_score.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var spinnerScoreValues: Spinner
-    private lateinit var tvScoreA: TextView
-    private lateinit var tvScoreB: TextView
+    private lateinit var binding: ActivityMainBinding
+
     private lateinit var switch: SwitchCompat
 
     // Array of integers to display score values in the spinner view
     private var scoreValues = arrayOf("Select Score Value", "1", "2", "3", "4", "5", "6")
 
+    // integer value to increase and decrease scores
+    private var valueToIncDec = 0
+
+    // Set default scoring team as A
+    private var isScoringTeamA = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Function to initialize views with their IDs.
-        initViews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Create the adapter to show dropdown menu
         val adapter = ArrayAdapter(this, R.layout.score_dropdown_menu, scoreValues)
         // Setting the adapter to the instance of Spinner View
-        spinnerScoreValues.adapter = adapter
+        binding.spinnerScoreValues.adapter = adapter
 
-        // Set the initial score to both the teams as "0"
-        tvScoreA.text = "0"
-        tvScoreB.text = "0"
+        binding.spinnerScoreValues.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                when (p2) {
+                    0 -> printToastMessage("Please select any score value.")
+                    else -> {
+                        valueToIncDec = scoreValues[p2].toInt()
+                    }
+                }
+            }
+        }
+
+        // Set the initial score to both the teams as var "valueToIncDec"
+        binding.tvScoreA.text = valueToIncDec.toString()
+        binding.tvScoreB.text = valueToIncDec.toString()
 
         // Switch change listener to pass message when switch changes it's state
         switch.setOnCheckedChangeListener { _, isChecked ->
             val message: String = if (isChecked) "You have selected Team B for scoring"
             else "You have selected Team A for scoring"
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-        }
 
+            isScoringTeamA = !isChecked
+            printToastMessage(message)
+        }
     }
 
-    private fun initViews() {
-        spinnerScoreValues = findViewById(R.id.spinner_score_values)
-
-        tvScoreA = findViewById(R.id.tv_score_a)
-        tvScoreB = findViewById(R.id.tv_score_b)
-
-        switch = findViewById(R.id.switch_to_change)
+    // Function to print Toast Message
+    private fun printToastMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
